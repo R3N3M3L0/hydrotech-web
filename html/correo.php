1e051b7453c258c328e9c6e7f6dfe01fd1e0f7ec
R3N3M3L0 <95933679+R3N3M3L0@users.noreply.github.com>
2022-01-05 19:21:14 -0500

    confirmar

--- html/correo.php
+++ html/correo.php
@@ -1,7 +1,7 @@
 <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
+$mensaje = $_POST['msg'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -10,13 +10,14 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['msg'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'renemelo@hotmail.com';
+$para = 'renemeloco@hotmail.com';
 $asunto = 'Mensaje de mi sitio web';
+$email = mail($para, $asunto, utf8_decode($mensaje), $header);
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
-header("Location:../index.html");
+if ($email) {
+    header("Location:agradecimiento.html");
+}
 ?>
